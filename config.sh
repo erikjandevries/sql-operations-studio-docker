7f6cbee86ca268e060f44640c89ed40287c30e35
Erik Jan de Vries <erikjandevries@hotmail.com>
2018-06-22 10:40:20 +0200

    Prepare data folders with user permissions

--- config.sh
+++ config.sh
@@ -7,3 +7,11 @@ docker_repository="erikjandevries"
 docker_image_id="${docker_repository}/${docker_image_name}:${docker_image_version}"
 
 mount_dir="/mnt"
+
+# Prepare data folders
+sudo mkdir -p $HOME/.docker-apps/sqlops
+sudo chown $USER:$USER $HOME/.docker-apps/sqlops
+mkdir -p $HOME/.docker-apps/sqlops/.config
+mkdir -p $HOME/.docker-apps/sqlops/.pki
+mkdir -p $HOME/.docker-apps/sqlops/.sqlops
+mkdir -p $HOME/.docker-apps/sqlops/.sqlsecrets
